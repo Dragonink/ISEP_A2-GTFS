ffb20ee1b5d4016ef6c6d79c97a9bf25391521d8
ThibaultdeCourson <thibault.db.decourson@gmail.com>
2021-01-06 21:57:08 +0100

    4. Implemented: clusters detection

--- src/clustering.py
+++ src/clustering.py
@@ -5,43 +5,62 @@ def clustering(DIJKSTRA: Pathfinder, nodes, n):
     """
     Clustering method (first try)
     """
+    n_clusters = 0
 
-    edge_betweenness = {}
+    while n_clusters < n:
+        edge_betweenness = {}
 
-    highest_betweenness = ["",0]
+        nodes_found = set()
+        n_clusters = 0
 
-    while nodes:
-        print(len(nodes), "nodes to explore left")
-        starting_node = nodes.pop()
+        highest_betweenness = ["",0]
 
-        for target_node in nodes:
+        nodes_to_explore = nodes
 
-            path = DIJKSTRA.get_path(starting_node, target_node)
+        while nodes_to_explore:
+            print(len(nodes_to_explore), "nodes to explore left")
+            starting_node = nodes_to_explore.pop()
 
-            if path is not None:
-                last_node = path.pop()
+            if starting_node not in nodes_found:
+                n_clusters += 1
+            nodes_found.add(starting_node)
 
-                while path:
-                    previous_node = path.pop()
+            for target_node in nodes_to_explore:
 
-                    edge_name = tuple([previous_node, last_node])
-                    if edge_name in edge_betweenness:
-                        edge_betweenness[edge_name] += 1
+                path = DIJKSTRA.get_path(starting_node, target_node)
 
-                        if edge_betweenness[edge_name] > highest_betweenness[1]:
-                            highest_betweenness = [edge_name, edge_betweenness[edge_name]]
-                    else:
-                        edge_betweenness[edge_name] = 1
+                if path is not None:
 
-                    last_node = previous_node
+                    if target_node not in nodes_found:
+                        nodes_found.add(target_node)  # This node is in the same cluster, add it to the found set
 
-        #print("Temp edges betweenness:", edge_betweenness)
+                    last_node = path.pop()
 
-    #print("Edges betweenness:", edge_betweenness)
+                    while path:
+                        previous_node = path.pop()
 
-    """
-    Bon dans le fonctionnement actuel du programme il faudrait que je trouve un moyen de récupérer le poids de la
-    liaison pour pouvoir la trouver dans adjacency et la supprimer. De fait le poids ne devrait pas être compris dans
-    l'ID d'un edge du coup je laisse comme ça le temps que Tanguy modifie la structure du graph et du pathfinder
-    """
-    DIJKSTRA.graph.__adjacency.remove((edge_name[0], edge_name[1], 42))
+                        edge_name = tuple([previous_node, last_node])
+                        if edge_name in edge_betweenness:
+                            edge_betweenness[edge_name] += 1
+
+                            if edge_betweenness[edge_name] > highest_betweenness[1]:
+                                highest_betweenness = [edge_name, edge_betweenness[edge_name]]
+                        else:
+                            edge_betweenness[edge_name] = 1
+
+                        last_node = previous_node
+
+            #print("Temp edges betweenness:", edge_betweenness)
+
+        #print("Edges betweenness:", edge_betweenness)
+        print(n_clusters, "clusters found")
+
+        """
+        Bon dans le fonctionnement actuel du programme il faudrait que je trouve un moyen de récupérer le poids de la
+        liaison pour pouvoir la trouver dans adjacency et la supprimer. De fait le poids ne devrait pas être compris dans
+        l'ID d'un edge du coup je laisse comme ça le temps que Tanguy modifie la structure du graph et du pathfinder
+        """
+
+        DIJKSTRA.graph.__adjacency.remove((edge_name[0], edge_name[1], 42))
+
+    print(n, "clusters obtained")
