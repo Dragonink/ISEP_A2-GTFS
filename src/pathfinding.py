16b6834c38773bcc812ddedd2a9936dd627529c3
Dragonink <Dragonink@users.noreply.github.com>
2021-01-06 21:41:07 +0100

    Optimized IDs and neighbors computation Add PDF render of the report

--- src/pathfinding.py
+++ src/pathfinding.py
@@ -13,13 +13,13 @@ class Pathfinder:
 	- `method` - Function to compute shortest paths from a node
 	"""
 
-	def __init__(self, graph: Graph, method: Callable[['Pathfinder', str], None]):
+	def __init__(self, graph: Graph, method: Callable[['Pathfinder', int], None]):
 		self.graph = graph
-		self._previous: Dict[str, Dict[str, str]] = dict()
-		self._distance: Dict[str, Dict[str, float]] = dict()
+		self._previous: Dict[int, Dict[int, int]] = dict()
+		self._distance: Dict[int, Dict[int, float]] = dict()
 		self.__method = method
 
-	def has_path(self, u: str, v: str) -> bool:
+	def has_path(self, u: int, v: int) -> bool:
 		"""Check if a path exist between two nodes
 
 		# Arguments
@@ -33,7 +33,7 @@ class Pathfinder:
 			self.__method(self, u)
 		return v in self._previous[u]
 
-	def get_path(self, u: str, v: str):
+	def get_path(self, u: int, v: int):
 		"""Get the shortest path between two nodes
 
 		# Arguments
@@ -43,20 +43,19 @@ class Pathfinder:
 		# Return value
 		Ordered list of node keys; or `None` if a path does not exist
 		"""
-		if u not in self._previous:
-			self.__method(self, u)
 		if not self.has_path(u, v):
 			return None
-		path: List[str] = [v]
-		current = v
-		while current != u:
-			next = self._previous[u][current]
-			path.append(next)
-			current = next
-		path.reverse()
-		return path
+		else:
+			path: List[str] = [v]
+			current = v
+			while current != u:
+				next = self._previous[u][current]
+				path.append(next)
+				current = next
+			path.reverse()
+			return path
 
-	def get_distance(self, u: str, v: str) -> float:
+	def get_distance(self, u: int, v: int) -> float:
 		"""Get the distance between two nodes
 
 		# Arguments
@@ -71,7 +70,7 @@ class Pathfinder:
 		return self._distance[u][v] if v in self._distance[u] else inf
 
 
-def bfs(self: Pathfinder, v: str):
+def bfs(self: Pathfinder, v: int):
 	"""Breadth-First Search method for `Pathfinder`"""
 	if v not in self._previous:
 		self._previous[v] = dict()
@@ -80,25 +79,24 @@ def bfs(self: Pathfinder, v: str):
 		queue = [v]
 		while len(queue) > 0:
 			current = queue.pop(0)
-			for (u, _) in self.graph.neighbors_out(current):
+			for (u, _) in self.graph[current].neighbors_out:
 				if u not in self._previous[v]:
 					self._previous[v][u] = current
 					self._distance[v][u] = self._distance[v][current] + 1
 					queue.append(u)
 
-
-def dijkstra(self: Pathfinder, v: str):
+def dijkstra(self: Pathfinder, v: int):
 	"""Dijkstra method for `Pathfinder`"""
 	if v not in self._previous:
 		self._previous[v] = dict()
 		self._distance[v] = dict()
 		self._distance[v][v] = 0
-		marked: Set[str] = set()
+		marked: Set[int] = set()
 		queue = [v]
 		while len(queue) > 0:
 			current = queue.pop(0)
 			marked.add(current)
-			for (u, weight) in self.graph.neighbors_out(current):
+			for (u, weight) in self.graph[current].neighbors_out:
 				tentative_distance = self._distance[v][current] + weight
 				if u not in self._distance[v] or tentative_distance < self._distance[v][u]:
 					self._previous[v][u] = current
